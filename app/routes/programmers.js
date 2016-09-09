3303f84c4b7571e306e92448ab2a43c9d0e26b9f
Phillip Jones <phillipemersonjones@gmail.com>
2016-09-09 13:47:22 -0500

    created a component and another route  - programmers

---
+++ app/routes/programmers.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return ["Dennis Ritchie","Linus Torvalds","Bjarne Stroustrup","Tim Berners-Lee","Brian Kernighan","Donald Knuth", "Ken Thompson"]
+  }
+});
